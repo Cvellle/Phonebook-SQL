8cbc3cc80ba2f85a0ee2ca03148f1a9152ebf1a0
Cvellle <cuekisa@yahoo.com>
2018-06-18 20:33:01 +0200

    a
    a

--- php/data.php
+++ php/data.php
@@ -3,7 +3,7 @@
 $arr = array();
 $con=new mysqli("localhost","root","","phonebook");
 
-$sql = "SELECT * FROM telephon";
+$sql = "SELECT * FROM telephone_numbers";
 if($result = $con->query($sql) ){
     while($row = $result->fetch_assoc()){
         $arr[] = $row;
